b43e4c273f718a13f0625aaca3afdc59b75a1905
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-11-28 16:28:11 +0000

    Add Recipres app and Comment app

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipes
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipes)
+class RecipesAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Recipes(models.Model):
+    title = models.CharField(max_length=200, verbose_name=('Recipe'), unique=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    description = models.TextField()
+    preparation = models.CharField(max_length=500, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modefy_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = ('Recipes')
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+
+    recipes = models.ForeignKey(Recipes, on_delete=models.CASCADE, related_name='comment')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
