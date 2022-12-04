25c9de759efc80ec3b5ec1ddae87114b1bc1941e
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-12-04 13:57:56 +0000

    Add RecipeDetail to views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipes
 
 
@@ -8,3 +8,24 @@ class RecipesList(generic.ListView):
     queryset = Recipes.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipes.objects.filter(status=1)
+        recipes = get_object_or_404(queryset, slug=slug)
+        comments = recipes.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipes.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipes": recipes,
+                "comments": commments,
+                "liked": liked
+            },
+        )
