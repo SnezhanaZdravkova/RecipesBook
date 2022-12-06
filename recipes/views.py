82afe0093cfcb38448a7d727c1e479b3e73d5cef
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-12-06 09:22:20 +0000

    Add Comment imput and delete the m in comments line 28 in views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Recipes
+from .models import Recipes, Comment
 
 
 class RecipesList(generic.ListView):
@@ -15,7 +15,7 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipes.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        comments = recipe.comment.filter(approved=True).order_by('created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -25,7 +25,7 @@ class RecipeDetail(View):
             "recipe_detail.html",
             {
                 "recipe": recipe,
-                "comments": commments,
+                "comments": comments,
                 "liked": liked
             },
         )
