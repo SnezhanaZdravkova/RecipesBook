8cdc415b4b27cbdcaa8eb41e760c1cd4735cd47e
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-12-14 18:28:37 +0000

    Add delete Autor from model and admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Recipes, Comment, Author
+from .models import Recipes, Comment
 from django_summernote.admin import SummernoteModelAdmin
-
-
-admin.site.register(Author)
 
 
 @admin.register(Recipes)
