732d9b9e772fc61e1b648e8b1c6c99341a7729b1
SnezhanaZdravkova <>
2023-04-24 17:46:16 +0000

    Bug problem solved, CRUD working

--- recipes/views.py
+++ recipes/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import (
-LoginRequiredMixin,
-# UserPassesTestMixin
+    LoginRequiredMixin,
+    UserPassesTestMixin
 )
 from django.views import generic, View
 from django.views.generic import DetailView, CreateView, DeleteView, ListView
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 from django.db.models import Count
-# from django.utils.text import slugify
+from django.utils.text import slugify
 from django.http import HttpResponseRedirect
 from .models import Recipes, Comment
 from .forms import CommentForm, CreateRecipeForm
@@ -47,13 +47,13 @@ class RecipeDetail(DetailView):
                 "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
-            },
+            }
         )
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Recipes.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comment.filter(approved=True).order_by('created_on')
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -89,8 +89,8 @@ class EditComment(UpdateView):
     form_class = CommentForm
 
 
-def delete_comment(request, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return HttpResponseRedirect(reverse(
         'recipe_detail', args=[comment.recipe.slug]))
@@ -99,12 +99,12 @@ def delete_comment(request, comment_id):
 class RecipeLike(View):
 
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Recipes, slug=slug)
+        recipe = get_object_or_404(Recipes, slug=slug)
 
-        if post.likes.filter(id=request.user.id).exists():
-            post.likes.remove(request.user)
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.likes.remove(request.user)
         else:
-            post.likes.add(request.user)
+            recipe.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
 
@@ -128,31 +128,31 @@ class CreateRecipe(LoginRequiredMixin, CreateView):
         messages.success(self.request,
                          "Recipe Successfully Added & Awaiting Approval")
         form.instance.author = self.request.user
-        return super(CreateRecipe, self).form_valid(form)
+        return super(CreateView, self).form_valid(form)
 
 
-class UpdateRecipe(UpdateView):
+class UpdateRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """ Edit Recipe """
 
-    model = Recipes
     template_name = 'update_recipe.html'
+    model = Recipes
     form_class = CreateRecipeForm
     pk_url_kwarg = 'pk'
-    success_url = reverse_lazy('update_recipe')
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         messages.success(self.request, "Recipe Successfully Updated")
         form.instance.author = self.request.user
-        return super(UpdateRecipe, self).form_valid(form)
+        return super(UpdateView, self).form_valid(form)
 
     def test_func(self):
         recipe = self.get_object()
         return recipe.author == self.request.user
 
 
-def delete_recipe(request, recipes_id):
+def delete_recipe(request, pk):
     """ Delete recipe"""
-    recipe = get_object_or_404(Recipes, id=recipes_id)
+    recipe = get_object_or_404(Recipes, pk=pk)
     recipe.delete()
 
     return redirect(reverse('home'))
